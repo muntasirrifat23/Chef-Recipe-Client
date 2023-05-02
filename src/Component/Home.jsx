22b63ae63d6b63c3a953c5741602c144ae2939b3
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-05-03 05:15:26 +0600

    card create

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
-import { Container } from 'react-bootstrap';
-import Login from './Login/Login';
-
+import { Col, Container, Row } from 'react-bootstrap';
+import Page from './Page/Page';
 const Home = () => {
     return (
         <Container>
             <Header></Header>
+            <Row  className='register'>
+                <Col lg={3}>
+                </Col>
+                <Col lg={9}>
+                <Page></Page>
 
-            
+                </Col>
+            </Row>
+
             <Outlet></Outlet>
 
 
