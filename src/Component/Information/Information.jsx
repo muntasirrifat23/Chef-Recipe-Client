0e2c1537fdad2aff6fff8ee0241f04c0b895025c
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-05-04 03:09:13 +0600

    rating

--- src/Component/Information/Information.jsx
+++ src/Component/Information/Information.jsx
@@ -1,34 +1,51 @@
 import React from 'react';
 import './Info.css'
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
+import { Button, Card, Col, Row } from 'react-bootstrap';
+import { FaStarHalf, FaStar, FaRegStar } from 'react-icons/fa';
+import Rating from 'react-rating';
+
 const Information = ({ item }) => {
     const { pic_, name, experience, recipe, likes, title, description } = item;
 
     return (
         <div className='info'>
-            <div className="row row-cols-1 row-cols-md-2 g-4">
-                <div className="col">
-                    <div className="card">
-                        <img src={pic_} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h4>Food Name: {title}</h4>
-                            <h5 className="card-title">Chef Name: {name}</h5>
-                            <div className='d-flex justify-content-between mt-3 mb-3'>
-                                <h6>{experience} </h6>
-                                <h6> {likes}</h6>
-                            </div>
+            <Row xs={1} md={2} lg={3}>
+                <Col>
+                    <Card >
+                        <div className="col">
+                            <div className="card">
+                                <img src={pic_} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h3 className='text-success'>Food: {title}</h3>
+                                    <h5 className="card-title text-secondary">Chef Name: {name}</h5>
 
-                            <p>{description}..........
-                            
-                            <Link to="/chef"><div><Button className='bg-danger'>View More</Button></div></Link>
-                            
-                            </p>
-                                
+                                    <div className='d-flex justify-content-between mt-3 mb-3'>
+                                        <h6>{experience} </h6>
+
+                                        <div className='align-items-center'>
+                                        <h6>{likes}
+                                            <Rating placeholderRating={likes}
+                                                readonly
+                                                emptySymbol={<FaRegStar />}
+                                                placeholderSymbol={<FaStar />}
+                                                fullSymbol={<FaStar />}>
+
+                                            </Rating>
+                                        </h6>
+                                        </div>
+                                    </div>
+
+                                    <p>{description}..........
+                                        <Link to="/chef"><div><Button className='bg-danger' >View More</Button></div></Link>
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    </Card>
+                </Col>
+            </Row>
+
         </div>
     );
 };
