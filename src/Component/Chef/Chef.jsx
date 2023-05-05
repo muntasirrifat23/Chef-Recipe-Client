f8ab26e4a31b51fb259a90c92ab28d7849a488fd
Munshi Muntasir Rifat <www.muntasirrifat23@gmail.com>
2023-05-05 14:34:10 +0600

    log out

--- src/Component/Chef/Chef.jsx
+++ src/Component/Chef/Chef.jsx
@@ -1,10 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useContext, useEffect, useState } from 'react';
+import { Button, Container } from 'react-bootstrap';
 import Information from '../Information/Information';
+import { AuthContest } from '../Auth/AuthProvider';
+import { Link } from 'react-router-dom';
 
 const Chef = () => {
     const [items, setItem] = useState([]);
 
+    const {user,logOut} = useContext(AuthContest);
+    const handleOut=()=>{
+      logOut()
+      .then(result=>
+        console.log(result)
+      )
+      .catch(err=>
+        console.log(err.message)
+        )
+    }
     useEffect(() => {
       fetch('https://assignment-10-express-muntasirrifat23.vercel.app/data')
         .then(res => res.json())
@@ -13,6 +25,15 @@ const Chef = () => {
     }, [])
     return (
         <Container>
+          <div className='mb-4'>
+          {
+          user?
+          (<Button onClick={handleOut} className='bg-danger'>LogOut <span  className='text-dark'>
+            <span></span>{user?.email} 
+             </span> </Button>):
+          (<Link to='/login'></Link>)
+        }
+          </div>
         {
           items.map(item=> <Information
           key={item.id}
@@ -20,6 +41,7 @@ const Chef = () => {
   
           </Information>)
         }
+        
       </Container>
     );
 };
